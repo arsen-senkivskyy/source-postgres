2e059821059160b88f552e91c9dec6bc95e38e31
Arsen <arsen@panoply.io>
2021-02-16 17:37:29 +0200

    changed python-sdk version to 1.6.5

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from distutils.core import setup
+import os
+
+token = os.environ.get("GITAUTH", "")
 
 setup(
     name="panoply_postgres",
-    version="2.3.7",
+    version="2.3.8",
     description="Panoply Data Source for Postgres",
     author="Panoply Dev Team",
     author_email="support@panoply.io",
     url="http://panoply.io",
     install_requires=[
-        "panoply-python-sdk==1.6.0",
+        "panoply-python-sdk @ git+https://{}@github.com/panoplyio/"
+        "panoply-python-sdk.git@v1.6.5#egg==panoply-python-sdk".format(token),
         "psycopg2==2.7.1",
         "backoff==1.4.3"
     ],
